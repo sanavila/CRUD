355bb4b61f4472869621f05695b026ade09e491b
Rute <rute.leone@hotmail.com>
2021-12-29 18:13:31 -0300

    startando projeto

---
+++ crudnode/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const port = 8090;
+
+const cursos  = ['FullStack Master', 'Desenvolvimento de games', 'Viver de Youtube '];
+
+
+//CRUD create, read, update,  delete
+//retorna um curso 
+
+app.get('/cursos/:index', (req, res) => {
+    const {index} = req.params;
+    
+    return res.json(cursos[index]);
+});
+
+//retornar todos os cursos 
+
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
+
+// criar um novo curso 
+
+server.post('/cursos', (req, res) =>{
+    const {name} = req.body
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+//atualizar um curso
+
+server.put('/cursos/:index', (req, res) => {
+    const {index} = req.params;b
+    const  {name} = req.body;
+
+    cursos [index] = name;
+
+    return res.json(cursos);
+});
+
+server.delete('/cursos/:index', (req, res) => {
+    const {index} = req.params;
+
+    cursos.splice(index, 1);
+    return res.json({ message:"O curso foi deletado"})
+});
+
+server.listen(port);
